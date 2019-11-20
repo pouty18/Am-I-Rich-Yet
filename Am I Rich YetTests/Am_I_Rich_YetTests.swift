ad3bb0c1c83e1eaa0fd0fa512629d8ca17d1991b
Richard Poutier <rcpoutier@gmail.com>
2019-11-20 07:48:21 -0800

    Initial Commit

---
+++ Am I Rich YetTests/Am_I_Rich_YetTests.swift
@@ -0,0 +1,34 @@
+//
+//  Am_I_Rich_YetTests.swift
+//  Am I Rich YetTests
+//
+//  Created by Richard Poutier on 11/20/19.
+//  Copyright © 2019 richardpoutier. All rights reserved.
+//
+
+import XCTest
+@testable import Am_I_Rich_Yet
+
+class Am_I_Rich_YetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
