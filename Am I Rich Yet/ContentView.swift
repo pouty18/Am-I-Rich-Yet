4af76ed61097cc18ac94845b00c99c96f295e760
Richard Poutier <rcpoutier@gmail.com>
2019-11-20 08:07:59 -0800

    Updated view

--- Am I Rich Yet/ContentView.swift
+++ Am I Rich Yet/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputType = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Am I Rich Yet?")
+                    .font(.title)
+                Spacer()
+            }
+            Text("Total Revnue")
+            
+            Picker(selection: $inputType, label: Text("Select how you make money")) {
+                Text("Paid App").tag(0)
+                Text("Subscription").tag(1)
+                Text("In-app Purchase").tag(2)
+            }.pickerStyle(SegmentedPickerStyle())
+            
+            Spacer()
+            if inputType == 0 {
+                Text("Paid App Model")
+            } else if inputType == 1 {
+                Text("Subscription Model")
+            } else if inputType == 2 {
+                Text("In-app Purchase Model")
+            }
+            Spacer()
+        } .padding()
     }
 }
 
